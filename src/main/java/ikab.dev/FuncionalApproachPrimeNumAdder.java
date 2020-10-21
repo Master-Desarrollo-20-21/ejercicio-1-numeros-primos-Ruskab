539c40ada5ae5d8812b9f5cedad609d8c4011536
ruskab <aq0866sw1343de>
2020-10-21 20:28:29 +0200

    remove magical numbers

--- src/main/java/ikab.dev/FuncionalApproachPrimeNumAdder.java
+++ src/main/java/ikab.dev/FuncionalApproachPrimeNumAdder.java
@@ -7,16 +7,20 @@ import java.util.stream.IntStream;
 
 public class FuncionalApproachPrimeNumAdder {
 
+    public static final int PRIME_NUMBERS_COUNT_LIMIT = 50;
+    public static final int FIRST_PRIME_NUMBER = 2;
+    public static final int NUM_COUNT_LIMIT = 50;
+
     public int sumPrimeNumbersInFirst50Numbers() {
         // La suma de los números primos que hay entre los 50 primeros números
-        return IntStream.rangeClosed(1, 50).filter(this::isPrimeNumber).sum();
+        return IntStream.rangeClosed(1, NUM_COUNT_LIMIT).filter(this::isPrimeNumber).sum();
     }
 
     public int sumFirst50PrimeNumbers() {
         // La suma de los 50 primeros números primos
-        AtomicInteger counter = new AtomicInteger(2);
+        AtomicInteger counter = new AtomicInteger(FIRST_PRIME_NUMBER);
         List<Integer> primeNumbers = new ArrayList<>();
-        while (primeNumbers.size() < 50) {
+        while (primeNumbers.size() < PRIME_NUMBERS_COUNT_LIMIT) {
             int actualNumber = counter.getAndIncrement();
             if (isPrimeNumber(actualNumber)) {
                 primeNumbers.add(actualNumber);
@@ -27,13 +31,13 @@ public class FuncionalApproachPrimeNumAdder {
 
 
     protected boolean isPrimeNumber(int number) {
-        if (number < 2) {
+        if (number < FIRST_PRIME_NUMBER) {
             return false;
         }
-        if (number == 2) {
+        if (number == FIRST_PRIME_NUMBER) {
             return true;
         }
-        return IntStream.rangeClosed(2, number - 1).allMatch(i -> number % i != 0);
+        return IntStream.rangeClosed(FIRST_PRIME_NUMBER, number - 1).allMatch(i -> number % i != 0);
     }
 
 }
--- src/main/java/ikab.dev/POOApproachPrimeNumAdder.java
+++ src/main/java/ikab.dev/POOApproachPrimeNumAdder.java
@@ -4,10 +4,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class POOApproachPrimeNumAdder {
 
+    public static final int NUM_COUNT_LIMIT = 50;
+    public static final int FIRST_PRIME_NUMBER = 2;
+    public static final int PRIME_NUMBERS_COUNT_LIMIT = 50;
+
     public int sumPrimeNumbersInFirst50Numbers() {
         // La suma de los números primos que hay entre los 50 primeros números
         int result = 0;
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < NUM_COUNT_LIMIT; i++) {
             if (isPrimeNumber(i)) {
                 result += i;
             }
@@ -18,9 +22,9 @@ public class POOApproachPrimeNumAdder {
     public int sumFirst50PrimeNumbers() {
         // La suma de los 50 primeros números primos
         AtomicInteger primeNumberCounter = new AtomicInteger();
-        AtomicInteger numberCounter = new AtomicInteger(2);
+        AtomicInteger numberCounter = new AtomicInteger(FIRST_PRIME_NUMBER);
         int result = 0;
-        while (primeNumberCounter.get() < 50) {
+        while (primeNumberCounter.get() < PRIME_NUMBERS_COUNT_LIMIT) {
             int actualNumber = numberCounter.getAndIncrement();
             if (isPrimeNumber(actualNumber)) {
                 primeNumberCounter.getAndIncrement();
@@ -35,7 +39,7 @@ public class POOApproachPrimeNumAdder {
         if (number == 1) {
             return false;
         }
-        for (int i = 2; i < number; i++) {
+        for (int i = FIRST_PRIME_NUMBER; i < number; i++) {
             if (number % i == 0) {
                 return false;
             }
